79c96ae7c158609ce7e927ec6d9e0b7453e12064
Mateus Cavalcanti <mateuscavalcanti1607@gmail.com>
2023-05-14 18:06:53 -0300

    Added writeOFF method

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,11 +4,8 @@
 int main(void)
 {
   Sculptor s(30,30,30);
-
-  std::cout << "Teste" << std::endl;
+  s.setColor(1.1,0.1,0.1,0.9);
+  s.putSphere(20,20,20,10);
   s.writeOFF("file.off");
-  
-
-
   return 0;
 }
